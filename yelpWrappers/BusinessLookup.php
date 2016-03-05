e04503bda3653ed90532b6e39aa13829cc143747
Callum Kendrick <callumkendrick@hotmail.co.uk>
2016-03-05 07:33:36 +0000

    Improved documentation of yelpWrappers/BusinessLookup.php

--- yelpWrappers/BusinessLookup.php
+++ yelpWrappers/BusinessLookup.php
@@ -1,11 +1,22 @@
 <?php
     require_once("YelpApiRequest.php");
 
+    /**
+     * The BusinessLookup class abstracts calls to the yelp business api.
+     * Operations are almost identical to what can be found in the documentation
+     * for this which can be found at
+     * yelp.co.uk/developers/documentation/v2/business . see this documentation
+     * for more information.
+     */
     class BusinessLookup
     {
             private $params;
             private $businessId;
 
+            /**
+             * Constructor for a BusinessLookup object. businessId must be
+             * given.
+             */
             public function __construct($businessId)
             {
                 $this->params = array(
@@ -19,8 +30,16 @@
             }
 
             /*
-             * param $newParams is an associative array with the new values to be
-             * set.
+             * The $newParams parameter should be an associative array with
+             * index names matching api parameters (note that this does not
+             * include the businessId. use setBusinessId())
+             *
+             * Possible Parameters:
+             * cc - ISO 3166-1 alpha-2 country code (e.g. GB)
+             * lang - ISO 639 language code (default en)
+             * lang_filter - if true, filter business reviews by specified
+             *      language
+             * actionlinks - see api documentation
              */
             public function setParams($newParams)
             {
@@ -30,11 +49,19 @@
                 }
             }
 
+            /*
+             * Change the businessId of the BusinessLookup object.
+             */
             public function setBusinessId($businessId)
             {
                 $this->businessId = $businessId;
             }
 
+            /**
+             * Invokes search api and returns raw json. For a full rundown of
+             * the data structure of the json returned, see the API
+             * documentation. 
+             */
             public function executeFullJson()
             {
                 $url = "https://api.yelp.com/v2/business/$this->businessId";
