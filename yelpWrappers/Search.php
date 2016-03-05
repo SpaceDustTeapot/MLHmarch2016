2a701db11bae6d415a6f6a19a3753a39fbf43e64
Callum Kendrick <callumkendrick@hotmail.co.uk>
2016-03-05 07:22:54 +0000

    Better documented search yelp wrapper

--- yelpWrappers/Search.php
+++ yelpWrappers/Search.php
@@ -1,12 +1,24 @@
 <?php
     require_once("YelpApiRequest.php");
 
+    /**
+     * The search class abstracts calls to the yelp search api. Operations are
+     * almost identical to what can be found in the documentation for this which
+     * can be found at yelp.co.uk/developers/documentation/v2/search_api . see
+     * this documentation for more information.
+     */
     class Search
     {
         private $params;
 
         /*
-         * Constructor for search object. $location must be given.
+         * Constructor for search object. A location MUST be given.
+         *
+         * The $location parameter may take many forms.For a full run down, see
+         * the api specification.
+         *
+         * One way to use the location parameter is as a place name, post code
+         * or country, etc.
          */
         public function __construct($location)
         {
@@ -24,8 +36,21 @@
         }
 
         /*
-         * param $newParams is an associative array with the new values to be
-         * set.
+         * The $newParams parameter should be an associative array with index
+         * names matching api parameters.
+         *
+         * Possible Parameters:
+         * location - a location as specified in the full api documentation
+         * term - a search term to use when searching
+         * limit - the amount of businesses to be returned
+         * offset - offset to the returned businesses
+         * sort -
+         *          0 = best matched (default)
+         *          1 = distance
+         *          2 = highest rated
+         * category_filter - category to filter by, see api documentation
+         * radius_filter - search area in meters
+         * deals_filter - if true, exclusively search for businesses with deals
          */
         public function setParams($newParams)
         {
@@ -35,6 +60,10 @@
             }
         }
 
+        /**
+         * Invokes search api and returns raw json. For a full rundown of the
+         * data structure of the json returned, see the API documentation. 
+         */
         public function executeFullJson()
         {
             $url = "https://api.yelp.com/v2/search";
