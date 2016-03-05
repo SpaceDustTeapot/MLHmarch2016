f9c62b6bd48f626a93e39257c31a401cd928981e
SpaceDustTeapot <spacedustmailpot@openmailbox.org>
2016-03-05 01:01:04 +0000

    Simple chat works

--- js/response_ajax.js
+++ js/response_ajax.js
@@ -3,21 +3,27 @@ function sendchat(str)
 {
   if(str.length == 0)
  {
+	alert("strlen = zero")
 	document.getElementById("txt").innerHTML = "";
 	return;
  }
  else
  {
   	var xmlhttp = new XMLHttpRequest();
+
+	//xmlhttp.onreadyStatechange = function()
 	xmlhttp.onreadystatechange = function()
 	{
+	   	
+		alert("HTML READYSTATE:" + xmlhttp.readyState+" STATUS: "+xmlhttp.status);
 	 	if(xmlhttp.readyState == 4 && xmlhttp.status == 200)
 		{
-		  document.getElementById("chat").innerHTML = xmlhttp.responseText; 
+		  document.getElementById("chat").innerHTML = xmlhttp.responseText;
 		}
+		
 	};
 
-	xmlhttp.open("GET","updatechat.php?chat="+str,true);
+	xmlhttp.open("GET","http://127.0.0.1/MLGmarch2016/updatechat.php?chat="+str,true);
 	xmlhttp.send();
  }
 
---
+++ update.sh
@@ -0,0 +1,3 @@
+ #!/bin/sh
+cd ..
+sudo cp -r MLGmarch2016 /var/www/html
