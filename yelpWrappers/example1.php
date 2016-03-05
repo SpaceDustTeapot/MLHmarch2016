77952b922d06fe4cfb942d5c4bcd2046609fa748
Callum Kendrick <callumkendrick@hotmail.co.uk>
2016-03-05 07:05:22 +0000

    Added better commenting to yelpWrappers/example1.php so that it may be used to understand the yelp wrappers

--- yelpWrappers/example1.php
+++ yelpWrappers/example1.php
@@ -1,14 +1,31 @@
 <?php
+    /*
+     * Example script for demonstrating how to possibly use the yelp api
+     * wrappers.
+     */
+
+    /* Include Classes for required functionailty */
+    //Search class for finding businesses
     require_once("Search.php");
+    //BusinessLookup class for getting more information about a business given
+    //its id.
     require_once("BusinessLookup.php");
 
+    //Searches must have a location. This can be a name as a string as below.
     $search = new Search("Manchester");
+    //Parameters can be modified or edited using arrays according to the yelp
+    //api specification. This includes the location parameter.
     $search->setParams(array("term" => "curry"));
 
+    //ExecuteFullJson calls the api method to return raw json from the api
     $json = $search->executeFullJson();
-    $arrayMaybe = json_decode($json, true);
-    $bId = $arrayMaybe['businesses'][0]['id'];
+    //Decode the json how you like, I chose as an associative array here.
+    $associativeArray = json_decode($json, true);
+    //It is easier to see the structure of this data by referring to the yelp
+    //api documentation.
+    $bId = $associateArray['businesses'][0]['id'];
 
+    //Business lookup works similarly to search. A business ID is mandatory.
     $business = new BusinessLookup($bId);
     var_dump($business->executeFullJson());
 ?>
