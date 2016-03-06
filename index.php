487f25a7750babdee2a853e9dc3628bf3c39e48e
Adam <Adzwoolly@users.noreply.github.com>
2016-03-06 02:21:52 +0000

    Better formatted, supports enter key
    HTML and PHP layout/formatting improved
    Pressing the enter key while in text box will submit text

--- index.php
+++ index.php
@@ -4,31 +4,22 @@
 $connection = mysqli_connect("127.0.0.1","mlh","dankmaymay","MLH");
 $uniqueid = bin2hex(substr(md5(rand()), 0, 7));
 initsession($uniqueid,$connection);
-function initsession($hash,$con)
-{
-//create cookie
 
-	if(isset($_COOKIE['sesh']))
-	{
-
+function initsession($hash,$con){
+	//create cookie
+	if(!isset($_COOKIE['sesh'])){
+		setcookie("sesh",$hash,time() +2592000);
 	}
-	else
-	{
-	  setcookie("sesh",$hash,time() +2592000);
-	  $mysql = "INSERT INTO users  ";
-	}
-
 }
-
 ?>
 
 <html>
 	<head>
-	<!--<link rel="stylesheet" href="css/index.css">-->
-	<link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="css/newLook.css"/>
-	<script type="text/javascript" src="js/chat.js"></script>
-       <script type="text/javascript" src="js/response_ajax.js"></script>
+		<!--<link rel="stylesheet" href="css/index.css">-->
+		<link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css' />
+		<link rel="stylesheet" href="css/newLook.css" />
+		<script type="text/javascript" src="js/chat.js"></script>
+		<script type="text/javascript" src="js/response_ajax.js"></script>
 	</head>
 
 	<body onload="first_load()">
@@ -39,25 +30,25 @@ function initsession($hash,$con)
 
 			<main>
 				<form>
-				<div id="comment">
-				<textarea name="chatbox" readonly id="chat"  type="Comment" rows="30" cols="120"></textarea>
-				</div>
-				<div id="text">
-				<textarea type="text" id="txt" name="textinput" rows="3" cols="39"></textarea>
-				<input type="button" onclick="sendchat(textinput.value); alert('BANTER')" name="Send" value="submit"/>
-				<br/>
-				</div>
-				<div id="emojiinput">
-				<input type="image" src="Emojis/1.png">
-				<input type="image" src="Emojis/2.png">
-				<input type="image" src="Emojis/3.png">
-				<input type="image" src="Emojis/4.png">
-				<input type="image" src="Emojis/5.png">
-				<input type="image" src="Emojis/6.png">
-				</div>
-				<div id="closedinput">
+					<div id="comment">
+						<textarea name="chatbox" readonly id="chat"  type="Comment" rows="30" cols="120"></textarea>
+					</div>
+					<div id="text">
+						<textarea type="text" id="txt" name="textinput" onkeypress="checkSubmit(event);"></textarea>
+						<input type="button" onclick="sendChat(document.getElementById('txt').value);" name="Send" value="submit" />
+						<br />
+					</div>
+					<div id="emojiinput">
+						<input type="image" src="Emojis/1.png" />
+						<input type="image" src="Emojis/2.png" />
+						<input type="image" src="Emojis/3.png" />
+						<input type="image" src="Emojis/4.png" />
+						<input type="image" src="Emojis/5.png" />
+						<input type="image" src="Emojis/6.png" />
+					</div>
+					<div id="closedinput">
 
-				</div>
+					</div>
 				</form>
 			</main>
 		</div>
